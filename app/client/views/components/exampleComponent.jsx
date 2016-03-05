a1e36a1d4280ea76211670b5a9100f3d542a136f
Gibby <john.gibby@localmed.com>
2016-03-05 10:50:27 -0600

    example component

---
+++ app/client/views/components/exampleComponent.jsx
@@ -0,0 +1,24 @@
+// import 'npmPackage';
+
+// export default React.createClass({
+//   propTypes: {
+//     exampleString: React.PropTypes.string,
+//     exampleObject: React.PropTypes.object,
+//     exampleArray: React.PropTypes.array,
+//     exampleBoolean: React.PropTypes.bool,
+//     exampleFunction: React.PropTypes.func,
+//     exampleNumber: React.PropTypes.number,
+//     exampleObjectWithProperties: React.PropTypes.shape({
+//       exampleKeyString: React.PropTypes.string,
+//       exampleKeyBoolean: React.PropTypes.bool
+//     })
+//   },
+
+//   render() {
+//     return(
+//       <div>
+//         <h1>Example Component</h1>
+//       </div>
+//     );
+//   }
+// });
